4ee559df11ba1e7da466cfaaf48bd5d5400ef7d8
iqno <hefelle@gmail.com>
2017-11-14 20:42:52 +0100

    test

--- src/Content.jsx
+++ src/Content.jsx
@@ -6,17 +6,21 @@ class Content extends Component {
   render() {
     return (
       <div className="hello">
-      	{this.props.text.map((obj,idx) => <div key={`${obj.id}-${idx}`}>{JSON.stringify(obj)}</div>)}
+        {' '}
+        Hello
+        {this.props.text.map((obj, idx) => (
+          <div key={`${obj.id}-${idx}`}>{JSON.stringify(obj)}</div>
+        ))}
       </div>
     );
   }
 }
 
 Content.propTpyes = {
-	text: PropTypes.array,
+  text: PropTypes.array
 };
 Content.defaultProps = {
-	text: [],
+  text: []
 };
 
 export default Content;
